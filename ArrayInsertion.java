2e376b01f5d706ac8d638bb8c7f61e064867d747
Pratyusha Dhal <pratyusshaofficial@gmail.com>
2026-08-19 20:12:50 +0530

    Added new ArrayInsertion

---
+++ ArrayInsertion.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class ArrayInsertion {
+    public static int[] insertAtIndex(int[] arr, int index, int val) {
+        int n = arr.length;
+        int[] newArr = new int[n + 1];
+        for (int i = 0; i < index; i++) {
+            newArr[i] = arr[i];
+        }
+        newArr[index] = val;
+        for (int i = index; i < n; i++) {
+            newArr[i + 1] = arr[i];
+        }
+
+        return newArr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int index = 2;
+        int val = 90;
+
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        int[] result = insertAtIndex(arr, index, val);
+
+        System.out.println("After Insertion: " + Arrays.toString(result));
+    }
+}
